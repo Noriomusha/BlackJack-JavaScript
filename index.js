637e75646d881227a166c1916d9402735f29a7f2
Noriomusha <edward.i.alvarado@gmail.com>
2023-04-23 15:27:06 -0400

    blackjack logic
    add blackjack logic

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Create two variables, firstCard and secondCard. 
+let firstCard = Math.floor(Math.random() * 10) + 2
+let secondCard = Math.floor(Math.random() * 10) + 2
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+console.log(firstCard, secondCard)
+let sum = firstCard + secondCard;
+console.log(sum);
+
+// 3. Create a variable, hasBlackJack, and set it to false
